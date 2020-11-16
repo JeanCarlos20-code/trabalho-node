6a27568ac41a814e8177fae71a6ebe25de0bd8b2
Jean Carlos <jeancarlos272010@gmail.com>
2020-11-16 01:06:28 -0300

    Trabalho

--- src/rota.js
+++ src/rota.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Inicio from '../src/Pages/Inicio'
+import NaoEncontrado from '../src/pages/naoEncontrado'
 
 function Rotas() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route exact path="/" component={Inicio} />
+                <Route component={NaoEncontrado} />
             </Switch>
         </BrowserRouter>
     )
